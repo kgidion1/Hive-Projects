9563c397605c982f5aa8995a9583cf73011b16f3
kgidion1 <kgidion1@gmail.com>
2017-05-13 00:27:44 +0300

    change on missioner

--- Hive_mission080417/app/Missioner.php
+++ Hive_mission080417/app/Missioner.php
@@ -7,7 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Missioner extends Model
 {
     protected $fillable = [
-        'firstname','lastname','dob', 'email', 'contact', 'tribe',
-        'maritalStatus','churchZone','employmentStatus','nationality',
+        'firstname','lastname','email', 'contact'
     ];
 }
