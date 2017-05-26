0b5cb07c2a0cc341846eede41bb55c18d5b02539
kgidion1 <kgidion1@gmail.com>
2017-05-27 00:09:28 +0300

    updates on hive_mission

--- Hive_mission080417/public/js/inputs.js
+++
@@ -1,9 +0,0 @@
-/** Requires jQuery */
-$(function () {
-	/** enables the stylesheet.  If javascript is disabled, normal checkboxes are used */
-	$(document.body).addClass("styled-checkboxes");
-});
-$(document).on('change', 'input[type=checkbox]', function (e) {
-	/** Catches the "checked" attribute up with the state of the checkbox */
-	$(this).attr('checked', $(this).prop('checked'));
-});
